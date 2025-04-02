82a32b0c112e3386910c405505a2ea22b9bae834
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-04-03 01:07:37 +0530

    add clear cache route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache cleared successfully!";
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
