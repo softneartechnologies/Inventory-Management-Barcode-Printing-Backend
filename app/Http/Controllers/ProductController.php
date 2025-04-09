e72e8767d01401f5b9f76d837602a3b0837cff94
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-04-09 17:28:13 +0530

    update dns1d barcode

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,16 +7,16 @@ use App\Models\Location;
 use App\Models\Stock;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-// use Milon\Barcode\Facades\DNS1DFacade as DNS1D;
-// use Milon\Barcode\Facades\DNS2DFacade as DNS2D;
+use Milon\Barcode\Facades\DNS1DFacade as DNS1D;
+use Milon\Barcode\Facades\DNS2DFacade as DNS2D;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
 use DB;
 // use Milon\Barcode\DNS1D;
 // use Milon\Barcode\DNS2D;
-use Milon\Barcode\Facades\DNS1D;
-use Milon\Barcode\Facades\DNS2D;
+// use Milon\Barcode\Facades\DNS1D;
+// use Milon\Barcode\Facades\DNS2D;
 use App\Models\BarcodeSetting;
 
 
