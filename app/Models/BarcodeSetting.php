3d57c409ba00d0478f9a91428a306b5b78abb924
Sunil Vishwakarma <sunil.vis2051991@gmail.com>
2025-05-27 16:53:26 +0530

    update barcode setting

--- app/Models/BarcodeSetting.php
+++ app/Models/BarcodeSetting.php
@@ -8,8 +8,11 @@ class BarcodeSetting extends Model
 {
     //
     protected $fillable = [
-        'sku', 'product_name', 'description', 'units', 'category_id', 'sub_category_id',
-        'manufacturer', 'vendor_id', 'model', 'returnable', 'cost_price', 'selling_cost',
-        'weight', 'weight_unit', 'length', 'width', 'depth', 'measurement_unit',
+        'product_name', 'sku',
+        'barcode_number', 'category_id', 'sub_category_id', 'manufacturer',
+        'vendor_id', 'model', 'unit_of_measurement_category', 'description',
+        'returnable', 'commit_stock_check', 'inventory_alert_threshold', 'location_id',
+        'quantity', 'unit_of_measure', 'per_unit_cost', 'total_cost',
+        'opening_stock',
     ];
 }
